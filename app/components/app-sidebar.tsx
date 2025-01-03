563164626c0627d8b6f57aaa737bd7868f753f42
Fahad khan <fahadkhanxdj@gmail.com>
2025-01-03 19:12:00 +0500

    Project Upload
    Project Upload

---
+++ app/components/app-sidebar.tsx
@@ -0,0 +1,40 @@
+"use client"
+
+import * as React from "react"
+import { SquareTerminal, MonitorCog, Receipt, Boxes, Shield, Banknote, Calculator, UserPen, FolderKanban, Network, Wallet, Globe, SquareUser, Landmark, SmartphoneNfc } from 'lucide-react'
+import { useTheme } from "next-themes"
+
+import { NavMain } from "./nav-main"
+import { NavProjects } from "./nav-project"
+import { NavUser } from "./nav-user"
+import { TeamSwitcher } from "./team-switcher"
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarHeader,
+  SidebarRail,
+} from "@/components/ui/sidebar"
+
+import { userData, teamItems, navMainItems } from "./sidebar-data"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  const { theme } = useTheme()
+  const isDarkTheme = theme === "dark"
+  return (
+    <Sidebar collapsible="icon" {...props} className={isDarkTheme ? "dark" : ""}>
+      <SidebarHeader>
+        <TeamSwitcher teams={teamItems} />
+      </SidebarHeader>
+      <SidebarContent>
+        <NavMain items={navMainItems} />
+      </SidebarContent>
+      <SidebarFooter>
+        <NavUser user={userData} />
+      </SidebarFooter>
+      <SidebarRail />
+    </Sidebar>
+  )
+}
+
+
---
+++ app/components/sidebar-data.ts
@@ -0,0 +1,132 @@
+import { Boxes, Shield, Banknote, Calculator, PenIcon as UserPen, FolderKanban, Network, Wallet, Globe, SquareUser, Landmark, SmartphoneNfc, SquareTerminal, Receipt, MonitorIcon as MonitorCog } from 'lucide-react'
+
+export const userData = {
+  name: "Iris Admin",
+  email: "m@example.com",
+  avatar: "/avatars/shadcn.jpg",
+  role: "Administrator"
+}
+
+export const teamItems = [
+  {
+    name: "Insights",
+    logo: Boxes,
+    plan: "Enterprise",
+  },
+  {
+    name: "Master Portal",
+    logo: Shield,
+    plan: "Startup",
+  },
+  {
+    name: "Payments",
+    logo: Banknote,
+    plan: "Free",
+  },
+  {
+    name: "Merchant and POS",
+    logo: Calculator,
+    plan: "Free",
+  },
+  {
+    name: "Personalization",
+    logo: UserPen,
+    plan: "Free",
+  },
+  {
+    name: "Product Manager",
+    logo: FolderKanban,
+    plan: "Free",
+  },
+  {
+    name: "Distribution Network",
+    logo: Network,
+    plan: "Free",
+  },
+  {
+    name: "Wallet Management",
+    logo: Wallet,
+    plan: "Free",
+  },
+  {
+    name: "Remittance",
+    logo: Globe,
+    plan: "Free",
+  },
+  {
+    name: "Customer Management",
+    logo: SquareUser,
+    plan: "Free",
+  },
+  {
+    name: "Digital Banking",
+    logo: Landmark,
+    plan: "Free",
+  },
+  {
+    name: "Payment Gateway",
+    logo: SmartphoneNfc,
+    plan: "Free",
+  },
+]
+
+export const navMainItems = [
+  {
+    title: "ATMs",
+    url: "#",
+    icon: SquareTerminal,
+    isActive: true,
+    items: [
+      {
+        title: "Profiles",
+        url: "#",
+      },
+      {
+        title: "Schedule Downtime",
+        url: "#",
+      },
+      {
+        title: "Message",
+        url: "#",
+      },
+      {
+        title: "Templates",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "ATM Receipts",
+    url: "#",
+    icon: Receipt,
+    items: [
+      {
+        title: "Item1",
+        url: "#",
+      },
+      {
+        title: "Item2",
+        url: "#",
+      },
+      {
+        title: "Item3",
+        url: "#",
+      },
+    ],
+  },
+  {
+    title: "Configuration Groups",
+    url: "#",
+    icon: MonitorCog,
+    items: [
+      {
+        title: "Configuration Profile",
+        url: "#",
+      },
+      {
+        title: "TM Bank",
+        url: "#",
+      },
+    ],
+  },
+]
